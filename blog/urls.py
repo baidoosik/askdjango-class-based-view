9dda1c3f05c85ecde5e14f241e6573e74e57a819
baidoosik <qoentlr37@naver.com>
2017-10-26 22:43:19 +0900

    클래스 변수 재정의

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
+from . import views
+
 
 urlpatterns =[
-
+    url(r'^greeting/$', views.greeting, name='greeting'),
+    url(r'^greeting2/$', views.greeting2, name='greeting'),
+    url(r'^greeting3/$', views.greeting3, name='greeting'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.views import View
 # Create your views here.
+
+
+class GreetingView(View):
+    message = 'good day'
+
+    def get(self, *args, **kwargs):
+        return HttpResponse(self.message)
+
+greeting = GreetingView.as_view()
+
+
+class MorgingGreetingView(GreetingView):
+    # 클래스 변수 재정의
+    message = 'good morning'
+
+
+greeting2 = MorgingGreetingView.as_view()
+greeting3 = GreetingView.as_view(message='good night')
