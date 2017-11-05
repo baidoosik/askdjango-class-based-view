8c6dbb01eb950cc939a8fe701f900ec281de8cd3
baidoosik <qoentlr37@naver.com>
2017-11-06 00:44:36 +0900

    cbv로 post detail 뷰 구현

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns =[
     url(r'^greeting2/$', views.greeting2, name='greeting'),
     url(r'^greeting3/$', views.greeting3, name='greeting'),
     url(r'^post/edit/(?P<pk>\d+)/$', views.post_edit, name='post_edit'),
-    url(r'^post/list/$', views.post_list, name='post_list')
+    url(r'^post/list/$', views.post_list, name='post_list'),
+    url(r'^post/detail/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 from .models import Post
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
@@ -45,7 +45,7 @@ class View(object):
 
 # 재사용이 가능하도록 ,cbv 패텅으로 재구현
 
-@method_decorator(login_required, name='dispatch')
+
 class EditFormView(View):
     model = None
     success_url = None
@@ -113,3 +113,5 @@ class PostListView(ListView):
         raise NotImplementedError
 
 post_list = PostListView.as_view()
+
+post_detail = DetailView.as_view(model=Post)
